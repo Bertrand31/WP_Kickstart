88400ff0e33b8e53538ac24aacb733ce6bd312e2
Dev CUA <tech@commeunarbre.fr>
2015-04-27 11:26:37 +0200

    Conf Déplacement des fichiers, nettoyage

--- library/bones.php
+++ library/bones.php
@@ -126,14 +126,8 @@ function bones_scripts_and_styles() {
 
   if (!is_admin()) {
 
-		// modernizr (without media query polyfill)
-		wp_register_script( 'bones-modernizr', get_stylesheet_directory_uri() . '/library/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
-
 		// register main stylesheet
-		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
-
-		// ie-only style sheet
-		wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
+		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/built/css/style.css', array(), '', 'all' );
 
     // comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
@@ -141,12 +135,10 @@ function bones_scripts_and_styles() {
     }
 
 		//adding scripts file in the footer
-		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
+		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/built/js/scripts.min.js', array( 'jquery' ), '', true );
 
 		// enqueue styles and scripts
-		wp_enqueue_script( 'bones-modernizr' );
 		wp_enqueue_style( 'bones-stylesheet' );
-		wp_enqueue_style( 'bones-ie-only' );
 
 		$wp_styles->add_data( 'bones-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
 
---
+++ library/config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/lacrea.fr"
+css_dir = "built/css"
+sass_dir = "src/scss"
+images_dir = "built/img"
+javascripts_dir = "built/js"
+fonts_dir = "built/fonts"
+
+output_style = :expanded # :expanded or :nested or :compact or :compressed
+
+relative_assets = true
+
+# line_comments = false
