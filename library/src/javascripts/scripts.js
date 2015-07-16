2dcfa872aa08acdae168ffba48f29897dc77b8a2
Agence CommeUnArbre <tech@commeunarbre.fr>
2015-07-16 08:18:05 +0000

    Ajout 'use strict'; + Nettoyage commentaires - Bitbucket

--- library/src/javascripts/scripts.js
+++ library/src/javascripts/scripts.js
@@ -1,17 +1,4 @@
-/*
- * Bones Scripts File
- * Author: Eddie Machado
- *
- * This file should contain any js scripts you want to add to the site.
- * Instead of calling it in the header or throwing it inside wp_head()
- * this file will be called automatically in the footer so as not to
- * slow the page load.
- *
- * There are a lot of example functions and tools in here. If you don't
- * need any of it, just remove it. They are meant to be helpers and are
- * not required. It's your world baby, you can do whatever you want.
-*/
-
+'use strict';
 
 /*
  * Get Viewport Dimensions
@@ -24,7 +11,6 @@ function updateViewportDimensions() {
 }
 // setting the viewport width
 var viewport = updateViewportDimensions();
-
 
 /*
  * Throttle Resize-triggered Events
@@ -46,11 +32,6 @@ var timeToWaitForLast = 100;
 
 /*
  * Here's an example so you can see how we're using the above function
- *
- * This is commented out so it won't work, but you can copy it and
- * remove the comments.
- *
- *
  *
  * If we want to only do it on a certain page, we can setup checks so we do it
  * as efficient as possible.
@@ -82,8 +63,6 @@ var timeToWaitForLast = 100;
  *
  * Pretty cool huh? You can create functions like this to conditionally load
  * content and other stuff dependent on the viewport.
- * Remember that mobile devices and javascript aren't the best of friends.
- * Keep it light and always make sure the larger viewports are doing the heavy lifting.
  *
 */
 
