b6db387d30ddf2cd06ae8ee51df43b2fe45de19f
Chafouin <tech@commeunarbre.fr>
2015-08-05 12:52:44 +0200

    Ajout gem normalize-scss

--- library/config.rb
+++ library/config.rb
@@ -1,14 +1,15 @@
 # Require any additional compass plugins here.
+require 'normalize-scss'
 
 # Set this to the root of your project when deployed:
-http_path = "/lacrea.fr"
+http_path = "/"
 css_dir = "built/css"
 sass_dir = "src/scss"
 images_dir = "built/img"
 javascripts_dir = "built/js"
 fonts_dir = "built/fonts"
 
-output_style = :expanded # :expanded or :nested or :compact or :compressed
+output_style = :expanded # :expanded, :nested, :compact or :compressed
 
 relative_assets = true
 
