764bdbe0e9cfdf79cf362d5649b80988d14b9583
Agence Comme un Arbre ! <tech@commeunarbre.fr>
2016-02-22 11:38:20 +0000

    Renommage de scripts.min.js en scripts.js

--- library/bones.php
+++ library/bones.php
@@ -129,7 +129,7 @@ function bones_scripts_and_styles() {
         }
 
         //adding scripts file in the footer
-        wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/built/js/scripts.min.js', array( 'jquery' ), '', true );
+        wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/built/js/scripts.js', array( 'jquery' ), '', true );
 
         // enqueue styles and scripts
         wp_enqueue_style( 'bones-stylesheet' );
